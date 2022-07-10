30500dc442d4543a844139999a0f6e68d2ebecbc
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-07-10 02:36:19 +0100

    hope

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  darkMode:'class',
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
+  plugins: [require("tw-elements/dist/plugin"), require("daisyui")],
+};
